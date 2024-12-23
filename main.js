a59af00f37ac42eae84e70c8e7b66106ac23237d
nellygreen <nellygreen.cg@gmail.com>
2024-12-23 20:57:45 +0100

    added js

---
+++ main.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    //Events on submit
+
+    form.addEventListener('submit', (e) => {
+
+        //Prevent page refresh when submitting form
+        e.preventDefault();
+
+        //Setup submit
+        const task = input.value;
+
+        if (!task) {
+            alert("Please fill out the task");
+            return;
+        }
+        
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
+
+        task_el.appendChild(task_content_el);
+
+        list_el.appendChild(task_el);
+
+    })
+
+})
